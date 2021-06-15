8fcbea471bb12c123284a627896ae21aadb630a7
dipon44 <dipon232@gmail.com>
2021-06-15 14:17:58 +0600

    2nd Commit

---
+++ PracticeProject/TestData/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Enter a Number : ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You look like {0} :) ",x);
+            Console.ReadLine();
+        }
+    }
+}
