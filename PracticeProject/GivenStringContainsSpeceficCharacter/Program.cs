eac52879d2104641fa507cd6dfa00d10cdbb1030
Dipon Kumar Das <dipon232@gmail.com>
2021-11-26 17:47:01 +0600

    2 Problems_sloved

---
+++ PracticeProject/GivenStringContainsSpeceficCharacter/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+//No 41 GivenString Contains Specefic Character
+
+Console.WriteLine("Give an Word : ");
+string input = Console.ReadLine();
+
+ 
+char[] charArray = input.ToCharArray();
+
+for (int i = 0; i < charArray.Length; i++)
+{
+    Console.WriteLine(charArray[i]);
+}
+
+ for(int i = 0; i < charArray.Length; i++)
+{
+    
+}
